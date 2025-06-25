e22bd9a3ba7f5e0928aa16cb85bc3cd4c55cec1a
GaneshRawool0718 <ganesh.rawool@nonstopio.com>
2025-06-25 22:05:48 +0530

    feat(Even_odd): checks if a number is even or odd

---
+++ data/Even_Odd.java
@@ -0,0 +1,20 @@
+package data;
+class EvenOdd {
+    // This method checks if a number is even or odd
+    // It returns true if the number is even, false if it is odd
+    
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        int number = 10; // Example number
+        if (isEven(number)) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+
+    
+}
